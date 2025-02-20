e9a1f85c85e9edde3d1ea44700bb18a6c38a773d
ddelre01 <diegodelreal11@gmail.com>
2025-02-20 13:14:40 -0800

    updating passing in params from run3dofsim.

--- run_3DoF_SIM.m
+++ run_3DoF_SIM.m
@@ -5,6 +5,22 @@ clc
 %% Load in Aircraft Parameters 
 load("Aircraft Params\cea308.mat") 
 J = 33.03; 
-states_init = [0 5000 0 40 0 0];
+gravity = 9.807; 
+
+
+fields = fieldnames(cea308); % Get all field names
+disp(fields)
+for i = 1:length(fields)
+    assignin('base', fields{i}, cea308.(fields{i})); % Assign each field to a variable
+end
+
+
+%% Set Initial States + Controls 
+states_init = [0 5000 0 10 0 0]; 
 deltaE = -10; 
+
+
+
+
+%% Open Simulink Model
 Iscold_Takeoff_Model
