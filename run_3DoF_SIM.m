28e6c0816a507a4c2de567e907331d8ecf384594
ddelre01 <diegodelreal11@gmail.com>
2025-02-19 16:12:54 -0800

    weight fix (kgf)? updating plotting

--- run_3DoF_SIM.m
+++ run_3DoF_SIM.m
@@ -5,5 +5,6 @@ clc
 %% Load in Aircraft Parameters 
 load("Aircraft Params\cea308.mat") 
 J = 33.03; 
-states_init = [0 10000 0 85 0 0];
+states_init = [0 5000 0 40 0 0];
+deltaE = -10; 
 Iscold_Takeoff_Model
